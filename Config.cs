71a2b5d15b1d8973adac669e1ed98f649c87fce5
Dakusy <32799638+Dakusy@users.noreply.github.com>
2018-09-10 21:59:30 +0200

    0.2

--- Config.cs
+++ Config.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
 
 namespace BotDiscord
 {
     class Config
     {
+        private const string configFolder = "Resources";
+        private const string configFile = "config.json";
+
+        static BotConfig bot;
+
         static Config()
         {
-
+            if (!Directory.Exists(configFolder))
+                Directory.CreateDirectory(configFolder);
+            if(!File.Exists(configFolder + "/" + configFile))
+            {
+                bot = new BotConfig();
+                string json = JsonConvert.SerializeObject(bot, Formatting.Indented);
+                File.WriteAllText(configFolder + "/" + configFile, json);
+            }
+            else
+            {
+                string json = File.ReadAllText(configFolder + "/" + configFile);
+                var data = JsonConvert.DeserializeObject<BotConfig>(json);
+            }
         }
 
     }
