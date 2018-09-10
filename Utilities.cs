89061487146b0f49c515d4c0dfd13899bbb56936
Dakusy <32799638+Dakusy@users.noreply.github.com>
2018-09-10 21:29:39 +0200

    add json files

---
+++ Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace BotDiscord
+{
+    class Utilities
+    {
+        static Utilities()
+        {
+            string json = File.ReadAllText("SystemLang/alerts.json");
+        }
+    }
+}
